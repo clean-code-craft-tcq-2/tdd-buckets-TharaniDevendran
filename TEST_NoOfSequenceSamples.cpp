9054ed4c1a4d894b63736c7db8d31bd3dae67c7c
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-03-09 09:29:56 +0530

    Create TEST_NoOfSequenceSamples.cpp

---
+++ TEST_NoOfSequenceSamples.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+
+#include "test/catch.hpp"
+#include <stdio.h>
+
+ int CurrentSamples[7] = {3, 3, 5, 4, 10, 11, 12};
+
+TEST_CASE("Read No of sequence samples")
+{
+ TotalNoOfSamples=CheckNoOfSequenceSamples(CurrentSamples);
+ REQUIRE(TotalNoOfSamples == 2);
+}
