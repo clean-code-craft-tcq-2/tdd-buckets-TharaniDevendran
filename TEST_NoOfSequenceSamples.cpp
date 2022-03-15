9ccf4376b8c498c6767c41713281d924383b88ed
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-03-15 19:54:02 +0530

    Update TEST_NoOfSequenceSamples.cpp

--- TEST_NoOfSequenceSamples.cpp
+++ TEST_NoOfSequenceSamples.cpp
@@ -8,6 +8,7 @@ int CurrentSamples[7] = {3, 3, 5, 4, 10, 11, 12};
 
 TEST_CASE("Read No of sequence samples")
 {
- int TotalNoOfSamples=CheckNoOfSequenceSamples(CurrentSamples, sizeof(CurrentSamples)/sizeof(CurrentSamples[0]));
+ int NoOfSamples = sizeof(CurrentSamples)/sizeof(CurrentSamples[0]);
+ int TotalNoOfSamples=CheckNoOfSequenceSamples(CurrentSamples, NoOfSamples);
  REQUIRE(TotalNoOfSamples == 2);
 }
