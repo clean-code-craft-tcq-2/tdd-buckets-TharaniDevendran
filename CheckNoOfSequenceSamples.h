fa1f5ea1b588556e9fedb9dfa98544678eddce79
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-03-15 19:43:02 +0530

    Update CheckNoOfSequenceSamples.h

--- CheckNoOfSequenceSamples.h
+++ CheckNoOfSequenceSamples.h
@@ -1,1 +1,6 @@
-int CheckNoOfSequenceSamples(int* CurrentSamples);
+#include <stdio.h>      /* printf */
+#include <stdlib.h>     /* qsort */
+
+#define MAX_NUMBER_OF_READINGS 50
+
+extern int CheckNoOfSequenceSamples(int* CurrentSamples, int NumberOfSamples);
