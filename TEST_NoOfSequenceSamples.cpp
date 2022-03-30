c9887c0fa51e1028414e077d10f04b5b6c1ff130
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-03-30 10:39:21 +0530

    Update TEST_NoOfSequenceSamples.cpp

--- TEST_NoOfSequenceSamples.cpp
+++ TEST_NoOfSequenceSamples.cpp
@@ -17,3 +17,17 @@ TEST_CASE("Read No of sequence samples from an array of 12 consecutive samples")
  int TotalNoOfSamples=CheckNoOfSequenceSamples(CurrentAnalogBit, 12);
  REQUIRE(TotalNoOfSamples == 1);
 }
+
+TEST_CASE("Check the no of sequence sample in an invalid array")
+{
+ int CurrentAnalogBit[12] ={1, 4095, 3, 4, 5, 6, 7, 4096, 9, 10, 4093,4094};
+ int TotalNoOfSamples=CheckNoOfSequenceSamples(CurrentAnalogBit, 12);
+ REQUIRE(TotalNoOfSamples == 2);
+}
+
+TEST_CASE("Read No of sequence samples from an array of 12 consecutive Analogsamples")
+{
+ int CurrentAnalogBit[12] ={409, 818, 1228, 1637, 2047, 2456, 2866, 3275, 3685, 4094, 4503,4918};
+ int TotalNoOfSamples=CheckNoOfSequenceSamples(CurrentAnalogBit, 12);
+ REQUIRE(TotalNoOfSamples == 2);
+}
